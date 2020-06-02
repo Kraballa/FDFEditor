1ae9f7dc7a10e909c8025b35c87eb260913c687b
Vinzent Brömauer <vinz.corno@web.de>
2020-06-02 02:20:29 +0200

    implemented drag and drop for the tabitems

--- FDFEditor/MainWindow.xaml.cs
+++ FDFEditor/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace FDFEditor
 {
@@ -210,6 +211,34 @@ namespace FDFEditor
             tab.Content = new TextEditorTabItem("Scratchpad\n----------\n\n");
             MainTabControl.Items.Add(tab);
             MainTabControl.SelectedIndex = MainTabControl.Items.IndexOf(tab);
+        }
+
+        private void TabItem_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var tabItem = e.Source as TabItem;
+
+            if (tabItem == null)
+                return;
+
+            if (Mouse.PrimaryDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(tabItem, tabItem, DragDropEffects.All);
+            }
+        }
+        private void TabItem_Drop(object sender, DragEventArgs e)
+        {
+            var tabItemTarget = e.Source as TabItem;
+            var tabItemSource = e.Data.GetData(typeof(TabItem)) as TabItem;
+
+            if (!tabItemTarget.Equals(tabItemSource))
+            {
+                var tabControl = tabItemTarget.Parent as TabControl;
+                int sourceIndex = tabControl.Items.IndexOf(tabItemSource);
+                int targetIndex = tabControl.Items.IndexOf(tabItemTarget);
+
+                tabControl.Items.Remove(tabItemSource);
+                tabControl.Items.Insert(targetIndex, tabItemSource);
+            }
         }
     }
 }
