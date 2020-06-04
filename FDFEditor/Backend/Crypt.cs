31e75fc400a50860f92b2c162264f161a95ef19f
Vinzent Brömauer <vinz.corno@web.de>
2020-06-04 13:25:05 +0200

    refactored and named crypt keys to reflect their actual use

--- FDFEditor/Backend/Crypt.cs
+++ FDFEditor/Backend/Crypt.cs
@@ -15,15 +15,15 @@ namespace FDFEditor.Backend
             FDF2
         }
 
-        private static byte[][] FDF2Keys = new byte[4][]
+        private static byte[][] FDFSteamKeys = new byte[4][]
         {
             new byte[16]{202,144,216,211,188,214,244,180,174,194,188,101,40,22,189,183},
             new byte[16]{61,88,123,71,96,236,242,152,50,42,213,5,171,156,175,190},
             new byte[16]{77,60,63,80,18,58,69,119,137,140,117,143,126,158,17,135},
             new byte[16]{242,165,41,132,231,150,148,35,220,80,204,233,36,174,251,77}
         };
-        private static byte[][] FDF1Keys = new byte[4][]
-            {
+        private static byte[][] FDFOldKeys = new byte[4][]
+        {
             new byte[16]{79,14,42,91,9,12,143,221,62,193,178,163,byte.MaxValue,162,5,7},
             new byte[16]{28,91,61,0,5,4,127,187,204,45,195,212,170,241,242,248},
             new byte[16]{204,219,153,8,byte.MaxValue,250,154,184,199,109,227,171,202,253,254,250},
@@ -39,7 +39,7 @@ namespace FDFEditor.Backend
         public static Stream CryptBuffer(byte[] buffer, bool decrypt = true, bool fdf1 = false, int type = 2)
         {
             TripleDESCryptoServiceProvider cryptoServiceProvider = new TripleDESCryptoServiceProvider();
-            cryptoServiceProvider.Key = fdf1 ? FDF1Keys[type] : FDF2Keys[type];
+            cryptoServiceProvider.Key = fdf1 ? FDFOldKeys[type] : FDFSteamKeys[type];
             cryptoServiceProvider.Mode = CipherMode.ECB;
             ICryptoTransform decryptor = decrypt ? cryptoServiceProvider.CreateDecryptor() : cryptoServiceProvider.CreateEncryptor();
             return (Stream)new MemoryStream(decryptor.TransformFinalBlock(buffer, 0, buffer.Length));
@@ -61,7 +61,7 @@ namespace FDFEditor.Backend
         {
             byte[] inputBuffer = File.ReadAllBytes(FileName);
             TripleDESCryptoServiceProvider cryptoServiceProvider = new TripleDESCryptoServiceProvider();
-            cryptoServiceProvider.Key = fdf1 ? FDF1Keys[type] : FDF2Keys[type];
+            cryptoServiceProvider.Key = fdf1 ? FDFOldKeys[type] : FDFSteamKeys[type];
             cryptoServiceProvider.Mode = CipherMode.ECB;
             byte[] buffer = cryptoServiceProvider.CreateEncryptor().TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
             FileStream fileStream = File.Create(FileName);
